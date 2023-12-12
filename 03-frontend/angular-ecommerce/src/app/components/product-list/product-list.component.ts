6fd7d038e54c706e86b18f66ab9c878ab5666cdb
Luke Brandt <lbrandt@lottery.idaho.gov>
2023-12-12 16:43:48 -0700

    adds more products and navigation using api

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,27 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  //templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit{
     
   products: Product[] = [];
+  currentCategoryId: number =1;
 
-  constructor(private productService: ProductService){
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute){
 
   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() =>{
       this.listProducts();
+    });
+    
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the "id" param string. convert string to a number using "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else{
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }

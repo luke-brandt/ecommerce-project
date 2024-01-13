d3056ffd3036565a12255214bb5f2a54c655ea18
Luke <lukeb1995@gmail.com>
2024-01-13 13:12:34 -0700

    Adds security for order

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.luv2code.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        //protect endpoint /api/orders
+        http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+
+
+        // add CORS filters
+        http.cors(Customizer.withDefaults());
+
+        //add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                            new HeaderContentNegotiationStrategy());
+
+        //force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        //disable CSRF since we are not using Cookies for session tracking
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
+
--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 import {Routes, RouterModule, Router} from '@angular/router';
@@ -32,6 +32,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -91,7 +92,8 @@ const routes: Routes = [
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, {provide: OKTA_CONFIG, useValue: { oktaAuth }}],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: { oktaAuth }},
+              {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
